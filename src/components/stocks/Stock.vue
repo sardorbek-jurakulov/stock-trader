0fb7dc26adce49724973f02e7624ba821d019b33
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-21 09:44:50 +0500

    WIP

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,8 +3,8 @@
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panel-title">
-          Name
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
         </h3>
       </div>
       <div class="panel-body">
@@ -13,11 +13,14 @@
             type="number"
             class="form-control"
             placeholder="Quantity"
+            v-model="quantity"
           >
         </div>
         <div class="pull-right">
           <button
-            class="btn btn-success">
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0 || !Number.isInteger(quantity-0)">
           Buy
           </button>
         </div>
@@ -26,6 +29,36 @@
   </div>
 </template>
 
-<script></script>
+<script>
+  export default {
+    props: {
+      stock: {
+        type: Object,
+        required: true,
+      }
+    },
+    data() {
+      return {
+        quantity: 0,
+      }
+    },
+    watch: {
+      quantity() {
+        console.log(this.quantity <= 0 || !Number.isInteger(this.quantity));
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity, 
+        };
+        console.log(order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
 
 <style></style>
