1a8073743b2b9e421933a02c77e254ae1b7a9cc7
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-21 15:15:41 +0500

    WIP

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
         </ul>
         <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">End day</a></li>
+          <li><a href="#" @click="endDay">End day</a></li>
           <li class="dropdown">
             <a 
               href="#" 
@@ -34,10 +34,19 @@
 </template>
 
 <script>
+  import { mapActions } from 'vuex';
   export default {
     computed: {
       funds() {
         return this.$store.getters.funds;
+      }
+    },
+    methods: {
+      ...mapActions([
+        'randomizeStocks'
+      ]),
+      endDay() {
+        this.randomizeStocks();
       }
     }
   }
