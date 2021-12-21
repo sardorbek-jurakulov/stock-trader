8a7b4dd973b2253eb05c66fa7a50793da66a06b0
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-21 16:34:26 +0500

    WIP

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import VueRouter from 'vue-router';
 import App from './App.vue'
 import { routes } from './routes.js';
 import store from './store/store.js';
+import VueResource from 'vue-resource';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-7580b-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
